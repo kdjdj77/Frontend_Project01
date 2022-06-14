db2a7bd8462579c1f668bd57cb9578579d58702f
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2022-06-15 00:45:06 +0900

    a

--- project_01_KDJ/js/script.js
+++ project_01_KDJ/js/script.js
@@ -6,51 +6,68 @@ var last_y = 0; // 스크롤 마지막 하단 높이
 var onpage_on = true;
 var isScroll = false;
 
-$(document).ready(function(){
-	init();
-	// Scroll Event
-	$('body').on('scroll touchmove mousewheel', function(event) {	
-		
-		//console.log(current_idx);
-		if(last_y > $("html").scrollTop() && !onpage_on){
-			//원페이지 스크롤 진입
-			console.log(":: 원페이지 시작 ::");
-			onpage_on = true;
-			isScroll = false;
-		}
-		if(!onpage_on) return;
+$(document).ready(
+	function() {
+		init();
+		// Scroll Event
+		$('body').on('scroll touchmove mousewheel', 
+			function(event) 
+			{	
+			
+				//console.log(current_idx);
+				if(last_y > $("html").scrollTop() && !onpage_on){
+					//원페이지 스크롤 진입
+					console.log(":: 원페이지 시작 ::");
+					onpage_on = true;
+					isScroll = false;
+				}
+				if(!onpage_on) return;
 
-		//스크롤 이벤트 막기
-		event.preventDefault();
-		event.stopPropagation();		
-		if(isScroll) return; // 현재 스크롤이 동작중이면 종료
-		isScroll = true;		
-		var direction = event.originalEvent.wheelDelta; //마우스 휠 방향
-		var y = 0;
-		if(direction > 0){
-			// up
-			current_idx --;
-			if(current_idx < 0){current_idx = -1;}
-			y = current_idx * page_h;
-		}
-		else{
-			// down
-			current_idx ++;
-			if(current_idx > total_section){
-				current_idx = total_section;
-				onpage_on = false;
-				return;
+				//스크롤 이벤트 막기
+				event.preventDefault();
+				event.stopPropagation();		
+				if(isScroll) return; // 현재 스크롤이 동작중이면 종료
+				isScroll = true;		
+				var direction = event.originalEvent.wheelDelta; //마우스 휠 방향
+				var y = 0;
+				if(direction > 0)
+				{
+					// up
+					current_idx --;
+					if(current_idx < 0){current_idx = -1;}
+					y = current_idx * page_h;
+				}
+				else
+				{
+					// down
+					current_idx ++;
+					if(current_idx > total_section)
+					{
+						current_idx = total_section;
+						onpage_on = false;
+						return;
+					}
+					y = current_idx * page_h;		
+				}
+				$('body').animate({scrollTop: y}, 500, 
+					function()
+					{
+						isScroll=false;
+					}
+				);	
 			}
-			y = current_idx * page_h;		
-		}
-		$('body').animate({scrollTop: y}, 500, function(){isScroll=false;});	
-	});
-});
-$( window ).resize(function() {
+		);
+	}
+);
+$( window ).resize(
+	function() 
+	{
 	// 반응형
 	setHeight();
-});
-function init(){
+	}
+);
+function init()
+{
 	setHeight();
 	total_section = $('#onepage > section').length;
 	last_y = page_h * total_section;
